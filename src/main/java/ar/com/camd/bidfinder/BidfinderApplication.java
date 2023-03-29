63405f3c8f996bf5b6caff969190b86371211dda
cduarte <duarte.camd@gmail.com>
2023-03-29 11:29:25 -0300

    First commit.

---
+++ src/main/java/ar/com/camd/bidfinder/BidfinderApplication.java
@@ -0,0 +1,13 @@
+package ar.com.camd.bidfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/camd/bidfinder/BidfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.camd.bidfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

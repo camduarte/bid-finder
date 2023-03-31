0805352d15c19cdfc2187fe18b30816aa310b564
cduarte <duarte.camd@gmail.com>
2023-03-31 15:01:25 -0300

    I add home controller.

---
+++ src/main/java/ar/com/camd/bidfinder/model/Pedido.java
@@ -0,0 +1,52 @@
+package ar.com.camd.bidfinder.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+	
+	private String nombreProducto;
+	private  BigDecimal valorNegociado;
+	private LocalDate fechaEntrega;
+	private String urlProducto;
+	private String descripcion;
+	private String urlImagen;
+
+	public String getNombreProducto() {
+		return nombreProducto;
+	}
+	public void setNombreProducto(String nombreProducto) {
+		this.nombreProducto = nombreProducto;
+	}
+	public BigDecimal getValorNegociado() {
+		return valorNegociado;
+	}
+	public void setValorNegociado(BigDecimal valorNegociado) {
+		this.valorNegociado = valorNegociado;
+	}
+	public LocalDate getFechaEntrega() {
+		return fechaEntrega;
+	}
+	public void setFechaEntrega(LocalDate fechaEntrega) {
+		this.fechaEntrega = fechaEntrega;
+	}
+	public String getUrlProducto() {
+		return urlProducto;
+	}
+	public void setUrlProducto(String urlProducto) {
+		this.urlProducto = urlProducto;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getUrlImagen() {
+		return urlImagen;
+	}
+	public void setUrlImagen(String urlImagen) {
+		this.urlImagen = urlImagen;
+	}
+
+}

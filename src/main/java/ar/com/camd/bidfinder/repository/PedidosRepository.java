8534d92b775c037b3460b92865b71fc66adb577f
cduarte <duarte.camd@gmail.com>
2023-04-02 07:49:55 -0300

    I use the JpaRepository class.

--- src/main/java/ar/com/camd/bidfinder/repository/PedidosRepository.java
+++ src/main/java/ar/com/camd/bidfinder/repository/PedidosRepository.java
@@ -1,22 +1,10 @@
 package ar.com.camd.bidfinder.repository;
 
-import java.util.List;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ar.com.camd.bidfinder.model.Pedido;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
 
 @Repository
-public class PedidosRepository {
-
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	public List<Pedido> findAll() {
-		Query query = entityManager.createNativeQuery("SELECT * FROM bidfinder.pedido", Pedido.class);
-		return query.getResultList();
-	}
+public interface PedidosRepository extends JpaRepository<Pedido, Long> {
 }

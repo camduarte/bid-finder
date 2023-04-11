08e754ab93f4e49174d45f0e226d6b6957c4bdfc
cduarte <duarte.camd@gmail.com>
2023-04-11 14:48:17 -0300

    If an exception ocurrs we redirect to home.

--- src/main/java/ar/com/camd/bidfinder/controller/HomeController.java
+++ src/main/java/ar/com/camd/bidfinder/controller/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +40,10 @@ public class HomeController {
 		modelAndView.addObject("pedidos", pedidos);
 
 		return modelAndView;
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ModelAndView onError() {
+		return new ModelAndView("redirect:/home");
 	}
 }

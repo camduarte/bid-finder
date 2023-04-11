9d28b6f13e9cddb488061debae2e6308e7788bcf
cduarte <duarte.camd@gmail.com>
2023-04-11 14:40:04 -0300

    Now we can filter order by status.

--- src/main/java/ar/com/camd/bidfinder/controller/HomeController.java
+++ src/main/java/ar/com/camd/bidfinder/controller/HomeController.java
@@ -6,23 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import ar.com.camd.bidfinder.model.Pedido;
+import ar.com.camd.bidfinder.model.StatusPedido;
 import ar.com.camd.bidfinder.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 	
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
-	@GetMapping("/home")
+	@GetMapping
 	public String home(Model model) {
-		
+
 		List<Pedido> pedidos = pedidoRepository.findAll();
 		
 		model.addAttribute("pedidos", pedidos);
 
 		return "home";
+	}
+	
+	@GetMapping("/{estado}")
+	public ModelAndView porEstado(@PathVariable("estado") String estado) {
+		List<Pedido> pedidos = pedidoRepository.findByEstado(StatusPedido.valueOf(estado.toUpperCase()));
+
+		ModelAndView modelAndView = new ModelAndView("/home");
+		modelAndView.addObject("pedidos", pedidos);
+
+		return modelAndView;
 	}
 }
--- src/main/java/ar/com/camd/bidfinder/repository/PedidoRepository.java
+++ src/main/java/ar/com/camd/bidfinder/repository/PedidoRepository.java
@@ -1,10 +1,15 @@
 package ar.com.camd.bidfinder.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ar.com.camd.bidfinder.model.Pedido;
+import ar.com.camd.bidfinder.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByEstado(StatusPedido valueOf);
 }

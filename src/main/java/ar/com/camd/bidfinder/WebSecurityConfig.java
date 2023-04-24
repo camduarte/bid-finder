ae5d6b8f9b15b4a6e2325f57e8a27e044da6403e
cduarte <duarte.camd@gmail.com>
2023-04-24 14:32:40 -0300

    Luego del login el usuario es enviado al home.

--- src/main/java/ar/com/camd/bidfinder/WebSecurityConfig.java
+++ src/main/java/ar/com/camd/bidfinder/WebSecurityConfig.java
@@ -21,6 +21,7 @@ public class WebSecurityConfig {
 				)
 				.formLogin(form -> form
 						.loginPage("/login")
+						.defaultSuccessUrl("/home", true)
 						.permitAll())
 				.logout(logout -> logout                                                
 			            .logoutUrl("/logout"));

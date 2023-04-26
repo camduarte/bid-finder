ab6477fc2fd8767c98e0629dc76cd3b7702f45fb
cduarte <duarte.camd@gmail.com>
2023-04-26 16:48:27 -0300

    El home muestra los pedidos ordenados de más reciente a antiguo. Implemento paginación en el home.

--- src/main/java/ar/com/camd/bidfinder/controller/HomeController.java
+++ src/main/java/ar/com/camd/bidfinder/controller/HomeController.java
@@ -3,6 +3,8 @@ package ar.com.camd.bidfinder.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,10 +22,13 @@ public class HomeController {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@GetMapping
 	public String home(Model model) {
-		List<Pedido> pedidos = pedidoRepository.findByEstado(StatusPedido.ENTREGADO);
+		Sort sort = Sort.by("fechaEntrega").descending(); // Orden por fecha de más reciente a antiguo
+		PageRequest pagination = PageRequest.of(0, 10, sort); // Paginación
+
+		List<Pedido> pedidos = pedidoRepository.findByEstado(StatusPedido.ENTREGADO, pagination);
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
--- src/main/java/ar/com/camd/bidfinder/repository/PedidoRepository.java
+++ src/main/java/ar/com/camd/bidfinder/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package ar.com.camd.bidfinder.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,7 +14,7 @@ import ar.com.camd.bidfinder.model.StatusPedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-	List<Pedido> findByEstado(StatusPedido valueOf);
+	List<Pedido> findByEstado(StatusPedido status, Pageable pagination);
 
 	@Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username")
 	List<Pedido> findAllByUser(@Param("username") String username);
